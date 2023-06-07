d6ab27855915951cb4815c7ea9ab14bf6a5e77a8
Chris Mems <christopher.mems@.com>
2023-06-06 19:43:40 -0500

    set up the ui for the guessed words

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
 const HangmanWord = () => {
-  return (
-    <div>HangmanWord</div>
-  )
-}
+  const word = "test";
+  const guessedLetters = ['t']
 
-export default HangmanWord
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span style={{
+            visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden',
+          }}>{letter}</span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default HangmanWord;
