9131cbb46969231fc80fb5ced603e9f71d2dfae2
Chris Mems <christopher.mems@.com>
2023-06-08 20:59:44 -0500

    include a key for each part of the body

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,97 +1,146 @@
 const HEAD = (
-  <div style={{
-    width: '50px',
-    height: '50px',
-    borderRadius: '100%',
-    border: '10px solid black',
-    position: 'absolute',
-    top: '50px',
-    right: '-30px'
-  }}/>
-)
+  <div
+    key="head"
+    style={{
+      width: '50px',
+      height: '50px',
+      borderRadius: '100%',
+      border: '10px solid black',
+      position: 'absolute',
+      top: '50px',
+      right: '-30px'
+    }}
+  />
+);
 
 const BODY = (
-  <div style={{
-    width: '10px',
-    height: '100px',
-    background: 'black',
-    position: 'absolute',
-    top: '120px',
-    right: '0'
-  }}/>
-)
+  <div
+    key="body"
+    style={{
+      width: '10px',
+      height: '100px',
+      background: 'black',
+      position: 'absolute',
+      top: '120px',
+      right: '0'
+    }}
+  />
+);
 
 const RIGHT_ARM = (
-  <div style={{
-    width: '100px',
-    height: '10px',
-    position: 'absolute',
-    background: 'black',
-    top: '150px',
-    right: '-100px',
-    rotate: '-30deg',
-    transformOrigin: 'left bottom'
-  }}/>
-)
+  <div
+    key="right-arm"
+    style={{
+      width: '100px',
+      height: '10px',
+      position: 'absolute',
+      background: 'black',
+      top: '150px',
+      right: '-100px',
+      rotate: '-30deg',
+      transformOrigin: 'left bottom'
+    }}
+  />
+);
+
 const LEFT_ARM = (
-  <div style={{
-    width: '100px',
-    height: '10px',
-    position: 'absolute',
-    background: 'black',
-    top: '150px',
-    right: '10px',
-    rotate: '30deg',
-    transformOrigin: 'right bottom'
-  }}/>
-)
+  <div
+    key="left-arm"
+    style={{
+      width: '100px',
+      height: '10px',
+      position: 'absolute',
+      background: 'black',
+      top: '150px',
+      right: '10px',
+      rotate: '30deg',
+      transformOrigin: 'right bottom'
+    }}
+  />
+);
 
 const RIGHT_LEG = (
   <div
+    key="right-leg"
     style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: "-90px",
-      rotate: "60deg",
-      transformOrigin: "left bottom",
+      width: '100px',
+      height: '10px',
+      background: 'black',
+      position: 'absolute',
+      top: '210px',
+      right: '-90px',
+      rotate: '60deg',
+      transformOrigin: 'left bottom',
     }}
   />
-)
+);
 
 const LEFT_LEG = (
   <div
+    key="left-leg"
     style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: 0,
-      rotate: "-60deg",
-      transformOrigin: "right bottom",
+      width: '100px',
+      height: '10px',
+      background: 'black',
+      position: 'absolute',
+      top: '210px',
+      right: '0',
+      rotate: '-60deg',
+      transformOrigin: 'right bottom',
     }}
   />
-)
+);
 
-const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
 
 type HangmanDrawingProps = {
-  numberOfGuesses: number
-}
+  numberOfGuesses: number;
+};
 
-const HangmanDrawing = ({numberOfGuesses}: HangmanDrawingProps) => {
+const HangmanDrawing = ({ numberOfGuesses }: HangmanDrawingProps) => {
   return (
-    <div style={{ position: 'relative'}}>
+    <div style={{ position: 'relative' }}>
       {BODY_PARTS.slice(0, numberOfGuesses)}
-      <div style={{height: '50px', width: '10px', background: 'black', top: '0px', position:'absolute', right: '0px'}}/>
-      <div style={{height: '10px', width: '200px', background: 'black', marginLeft: '120px'}}/>
-      <div style={{height: '400px', width: '10px', background: 'black', marginLeft: '120px'}}/>
-      <div style={{height: '10px', width: '250px', background: 'black'}}/>
+      <div
+        key="noose"
+        style={{
+          height: '50px',
+          width: '10px',
+          background: 'black',
+          top: '0px',
+          position: 'absolute',
+          right: '0px'
+        }}
+      />
+      <div
+        key="beam"
+        style={{
+          height: '10px',
+          width: '200px',
+          background: 'black',
+          marginLeft: '120px'
+        }}
+      />
+      <div
+        key="stand"
+        style={{
+          height: '400px',
+          width: '10px',
+          background: 'black',
+          marginLeft: '120px'
+        }}
+      />
+      <div
+        key="base"
+        style={{
+          height: '10px',
+          width: '250px',
+          background: 'black'
+        }}
+      />
     </div>
-  )
-}
+  );
+};
+
 
 export default HangmanDrawing
